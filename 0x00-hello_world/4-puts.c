f55d0ad637017af5f74591237875ff053b26f41f
SaidLamghari <said.lmg@gmail.com>
2023-08-10 16:27:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Use the function puts
+ *
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
