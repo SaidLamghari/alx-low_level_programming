a482c053fe81c9dd32526683569eb52d3fffc2c1
SaidLamghari <said.lmg@gmail.com>
2023-08-11 18:55:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char cl = 'a';
+	char cu = 'A';
+
+	while (cl <= 'z')
+	{
+		putchar(cl);
+		cl++;
+	}
+	while (cu <= 'Z')
+	{
+		putchar(cu);
+		cu++;
+	}
+	putchat('\n');
+	return (0);
+}
