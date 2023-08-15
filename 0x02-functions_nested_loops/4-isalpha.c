c11d960c50cda53480c81ec83ada162e629eda4f
SaidLamghari <said.lmg@gmail.com>
2023-08-15 16:43:52 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the char
  * Return: 1 for lowercase or uppercase and 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
