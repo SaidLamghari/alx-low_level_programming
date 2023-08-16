d1b9d4e8ee648161e830f15def614a9a8bc14a15
SaidLamghari <said.lmg@gmail.com>
2023-08-16 09:43:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - start of program
+ * @num: the int for abs
+ * Return: Always 0
+ *
+ *
+ */
+int _abs(int num)
+{
+
+	if (num < 0)
+		num = num * (-1);
+	return (num);
+}
