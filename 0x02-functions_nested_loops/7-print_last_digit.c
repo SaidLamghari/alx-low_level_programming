db60a93db3444a4bfc5b6b13be7de6ba53e308d7
SaidLamghari <said.lmg@gmail.com>
2023-08-16 10:10:31 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - start of program
+ * @num: the int
+ * Return: Always 0
+ *
+ */
+int print_last_digit(int num)
+{
+	num = num % 10;
+	return (num);
+}
