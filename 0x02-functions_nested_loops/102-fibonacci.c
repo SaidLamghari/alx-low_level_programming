42b017566376a30ba4e9b2ee159f11a5df554c27
SaidLamghari <said.lmg@gmail.com>
2023-08-16 17:20:46 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Start of program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	int f1 = 0, f2 = 1, s;
+
+	for (c = 0; c <= 49; c++)
+	{
+		s = f1 + f2;
+		printf("%d", s);
+		f1 = f2;
+		f2 = s;
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
