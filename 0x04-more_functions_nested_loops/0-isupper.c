285f6b814b5788473d1f4b4ecb468e3ee3f9bd4c
SaidLamghari <said.lmg@gmail.com>
2023-08-17 06:06:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+ * int _isupper - start of program
+ * @c: numbre input
+ * Return: Always 1 for uppercase 0 otherwise
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
