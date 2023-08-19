78c63322585da9ecee3d447c35caec5732a55c91
SaidLamghari <said.lmg@gmail.com>
2023-08-18 17:30:22 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - start of the program
+ * @n: input numbre
+ *
+ */
+void print_number(int n)
+{
+	int i;
+	
+	i = n;
+	if (i < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if ( i / 10)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
+}
