6d23b91b04f336c649e34aa0a3448c55ccffbcef
SaidLamghari <said.lmg@gmail.com>
2023-08-21 11:17:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - start of the program that prints a string
+ * @str: input
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
