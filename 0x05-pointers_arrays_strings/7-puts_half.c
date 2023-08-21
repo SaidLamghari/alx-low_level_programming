7a23c2c6aa14054f2933b73e31b3fc1f5d6ad579
SaidLamghari <said.lmg@gmail.com>
2023-08-21 15:44:00 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - start of the program that prints half of a string
+ *
+ * @str: input of a string
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j <= i; j++)
+			_putchar(str[j]);
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j <= i; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
