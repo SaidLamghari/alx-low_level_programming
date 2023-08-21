37d8d8b2e717504d82c56dd4e1972c53ab69f27a
SaidLamghari <said.lmg@gmail.com>
2023-08-21 16:47:27 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,14 @@
  */
 void puts_half(char *str)
 {
-	int a, n, longi;
+	int a, n, i;
 
-	longi = 0;
+	i = 0;
 	for (a = 0; str[a] != '\0'; a++)
-		longi++;
-	n = (longi / 2);
-	if ((longi % 2) == 1)
-		n = ((longi + 1) / 2);
+		i++;
+	n = (i / 2);
+	if ((i % 2) == 1)
+		n = ((i + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
