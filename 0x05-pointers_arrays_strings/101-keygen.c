1d31671bae7081dc31161d43b293bb9c68b24720
SaidLamghari <said.lmg@gmail.com>
2023-08-22 16:11:14 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,29 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-/*
- */
-char getRandomChar(const char *charset, int charsetLength) {
+
+    char getRandomChar(const char *charset, int charsetLength) {
    return charset[rand() % charsetLength];
 }
 
-int main()
-{
-	srand(time(NULL));
-	const char *lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-	const char *uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const char *digitChars = "0123456789";
-	const char *specialChars = "!@#$%^&*()-_+=<>?";
-	const char *allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?";
-	int passwordLength = 12;
+int main() {
+    srand(time(NULL));
 
-	char password[passwordLength + 1];
-	for (int i = 0; i < passwordLength; i++)
-	{
-		const char *charset = allChars;
-		int charsetLength = strlen(charset);
-		password[i] = getRandomChar(charset, charsetLength);
-	}
-	password[passwordLength] = '\0';
-	return 0;
+    const char *lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const char *uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char *digitChars = "0123456789";
+    const char *specialChars = "!@#$%^&*()-_+=<>?";
+
+    const char *allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?";
+
+    int passwordLength = 12;
+
+    char password[passwordLength + 1];
+
+    for (int i = 0; i < passwordLength; i++) 
+    {
+        const char *charset = allChars;
+
+            int charsetLength = strlen(charset);
+        password[i] = getRandomChar(charset, charsetLength);
+    }
+
+    password[passwordLength] = '\0';
+    printf("Generated Password: %s\n", password);
+
+    return 0;
 }
