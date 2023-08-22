24da18ced61a58ee573d08ed2cd4c9a5cbd1bdd2
SaidLamghari <said.lmg@gmail.com>
2023-08-22 11:14:54 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,25 +6,25 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, neg;
-	unsigned int var;
-
+	int neg = 1, i, k;
+	unsigned int var = 0;
+	
 	i = 0;
-	k = 0;
-	neg = 1;
+	k = -1;
 	while (s[i])
 		i++;
-	for (j = 0; j <= i; j++)
-	{
-		if (s[j] == '-')
+
+	do {
+		if (*s == '-')
 			neg = neg * -1;
-		if (s[j] >= '0' && s[j] <= '9')
-			var = (var * 10) + s[j] - ('0');
-		if (s[j] <= '0' && s[j] >= '9')
+		else if (*s >= '0' && *s <= '9')
+			var = (var * 10) + (*s - '0');
+		else if (*s < '0' || *s > 9)
 			k++;
-	}
-	if (k == i)
+	} while (*s++);
+
+	if (i <= k)
 		return (0);
+
 	return (var * neg);
 }
-
