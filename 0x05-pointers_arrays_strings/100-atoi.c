e4d4977c13878f0f28a80e10f8595fdaae9124e7
SaidLamghari <said.lmg@gmail.com>
2023-08-22 11:22:50 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,29 +2,23 @@
 /**
  * _atoi - start of the program that convert a string to an integer.
  * @s: input string
- * Return: numbre or 0
+ * Return: number
  */
 int _atoi(char *s)
 {
-	int neg = 1, i, k;
+	int neg = 1;
 	unsigned int var = 0;
-	
-	i = 0;
-	k = -1;
-	while (s[i])
-		i++;
+
 
 	do {
 		if (*s == '-')
 			neg = neg * -1;
 		else if (*s >= '0' && *s <= '9')
 			var = (var * 10) + (*s - '0');
-		else if (*s < '0' || *s > 9)
-			k++;
+		else if (var > 0)
+			break;
 	} while (*s++);
 
-	if (i <= k)
-		return (0);
 
 	return (var * neg);
 }
