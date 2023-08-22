177cf3c2586d3386346513d3ade11b6a9b3dd2d8
SaidLamghari <said.lmg@gmail.com>
2023-08-22 16:05:45 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+ */
+char getRandomChar(const char *charset, int charsetLength) {
+   return charset[rand() % charsetLength];
+}
+
+int main()
+{
+	srand(time(NULL));
+	const char *lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+	const char *uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char *digitChars = "0123456789";
+	const char *specialChars = "!@#$%^&*()-_+=<>?";
+	const char *allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=<>?";
+	int passwordLength = 12;
+
+	char password[passwordLength + 1];
+	for (int i = 0; i < passwordLength; i++)
+	{
+		const char *charset = allChars;
+		int charsetLength = strlen(charset);
+		password[i] = getRandomChar(charset, charsetLength);
+	}
+	password[passwordLength] = '\0';
+	return 0;
+}
