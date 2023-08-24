cdb4c1b826313e01bc656835ffc5340086e35047
SaidLamghari <said.lmg@gmail.com>
2023-08-24 10:56:56 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *leet - start of the program that encodes a string into 1337
+ * @l: string
+ *
+ * Return: encoded string
+ */
+char *leet(char *l)
+{
+	int i, j;
+	char *qa = "AEOTL";
+	char *la = "aeotl";
+	char *nu = "43071";
+
+	i = 0;
+	while (l[i])
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (l[i] == qa[j])
+				l[i] = nu[j];
+			if (l[i] == la[j])
+				l[i] = nu[j];
+		}
+		i++;
+	}
+	return (l);
+}
