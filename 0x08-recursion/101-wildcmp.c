8aa3eb5ef86a96e88523ab76c065e480f050510c
SaidLamghari <said.lmg@gmail.com>
2023-08-31 05:05:51 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * wildcmp - start of a function that compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else
+		return (0);
+}
