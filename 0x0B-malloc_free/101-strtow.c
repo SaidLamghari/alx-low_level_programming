1f1691aec05da1a5e124ce936d88e1b7c36ba1fa
SaidLamghari <said.lmg@gmail.com>
2023-09-05 19:27:17 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * **strtow - Start of function that splits a string into words.
+ * @str: pointer
+ * Return: Null or word
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || *str == '\0')
+		return (NULL);
+}
