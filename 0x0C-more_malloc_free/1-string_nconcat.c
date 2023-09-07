09934eb57af8da9b9b8c899df2a62bf39a1a9700
SaidLamghari <said.lmg@gmail.com>
2023-09-07 07:28:28 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,6 +29,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 	for (i = 0; i < n; i++)
 		str[len1 + i] = s2[i];
-	str[len1 + i] = '\n';
+	str[len1 + i] = '\0';
 	return (str);
 }
