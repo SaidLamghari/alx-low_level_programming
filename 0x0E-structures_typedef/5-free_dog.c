ec0b8eb376897c9d5d251fd061ec627ffb65175a
SaidLamghari <said.lmg@gmail.com>
2023-09-11 20:09:38 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
-
-
+/**
+ * free_dog - Start of function that frees dogs.
+ * @d: Dog
+ */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
 		free(d->owner);
 		free(d->name);
-		free(d);}
+		free(d);
+	}
 }
