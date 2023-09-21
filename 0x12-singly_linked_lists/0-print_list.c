f7d7b94fee04aee4790683a62062f701536b817c
SaidLamghari <said.lmg@gmail.com>
2023-09-21 08:16:39 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,8 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * print_list - Start of function that prints all the elements of a list_t list.
+ * print_list - Start of function that
+ *		prints all the elements of a list_t list.
  * @h: Pointer
  * Return: The number of nodes
  */
