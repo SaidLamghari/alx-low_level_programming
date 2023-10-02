40a01a7fc8926971103374896b43bcf82e1b64b6
SaidLamghari <said.lmg@gmail.com>
2023-10-02 09:53:21 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 	i = 0;
 	while (text_content[i])
 		i++;
+	
+	if (i == 0)
+		return (-1);
 
 	t_w = write(file, text_content, i);
 	if (t_w == -1)
