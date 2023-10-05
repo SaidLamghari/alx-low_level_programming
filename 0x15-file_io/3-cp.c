1195c66dfbf96f78fe1c9a000f75d51d5efb73e9
SaidLamghari <said.lmg@gmail.com>
2023-10-05 09:39:23 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#define RESUME1 (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
  * main - Start of the function that appends text at the end of a file.
  * @argc: Argument
@@ -10,7 +11,7 @@
  * Return: 1 on success and -1 on failure
  */
 int main(int argc, char **argv)
-{	int from_file = 0, to_file = 0;
+{	int from_file = -1, to_file = -1;
 	char buffer[1024];
 	ssize_t t_r, t_w;
 
@@ -19,15 +20,14 @@ int main(int argc, char **argv)
 	from_file = open(argv[1], O_RDONLY);
 	if (from_file == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	to_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, RESUME1);
 	if (to_file == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((t_r = read(from_file, buffer, sizeof(buffer))) > 0)
 	{
 		t_w = write(to_file, buffer, t_r);
-		if (t_w != t_r)
+		if (t_w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (t_r == -1)
@@ -36,5 +36,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file), exit(100);
 	if (close(to_file) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_file), exit(100);
-	return (1);
+	return (0);
 }
