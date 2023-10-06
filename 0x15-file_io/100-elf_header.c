b88c73c3ade3e747f6e7e7e43c5bb8f7147a4969
SaidLamghari <said.lmg@gmail.com>
2023-10-06 15:31:43 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,7 +11,7 @@
  */
 int main(int ac, char **av)
 {
-	int file;
+	int file, i;
 	Elf64_Ehdr header;
 
 	if (argc != 2)
@@ -21,4 +21,19 @@ int main(int ac, char **av)
 	if (file == -1)
 		dprintf(STDERR_FILENO, "Cant open file : %s\n", av[1]), exit(98);
 
+
+
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	printf("\n");
+	
+	printf("  Class:                             ");
+		if (header->e_ident[EI_CLASS] == ELFCLASS64)
+			printf("ELF64\n");
+		else printf("ELF32\n");
+	printf("  Data:                              ");
+		if(header->e_ident[EI_DATA] == ELFDATA2LSB)
+		       printf("2's complement, little endian\n");
+		else printf("2's complement, big endian\n");
 }
