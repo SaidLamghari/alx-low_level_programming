40d4bcdc796183b3702ec23736a285ffdaa011dd
SaidLamghari <said.lmg@gmail.com>
2023-12-07 07:20:33 -0800

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - Start of function that prints all the elements of a dlistint_t list.
+ * @h: The pointer
+ * return: count
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+
+		count++;
+
+	}
+
+
+	return count;
+}
