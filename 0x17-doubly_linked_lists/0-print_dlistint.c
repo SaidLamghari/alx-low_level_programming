161fdfffa33723be20656bcfc2effe9ba4e0c1ec
SaidLamghari <said.lmg@gmail.com>
2023-12-07 07:38:54 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
  * print_dlistint - Start of function that prints
  *			all the elements of a dlistint_t list.
  * @h: The pointer
- * return: count
+ * Return: count
  */
 
 size_t print_dlistint(const dlistint_t *h)
