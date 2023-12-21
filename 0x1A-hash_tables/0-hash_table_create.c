92a7a55e371a976e14dcbfa59c55ac7b3b2bc3a9
SaidLamghari <said.lmg@gmail.com>
2023-12-21 14:16:11 -0800

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Start of  function that creates a hash table.
+ * @size: The size of array
+ * Return: A pointer (copy) or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int count;
+	hash_table_t *copy;
+
+
+	copy = malloc(sizeof(hash_table_t));
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+
+
+
+	copy->array = malloc(sizeof(hiash_node_t *) * size);
+	if (copy->array == NULL)
+	{
+		free(copy), return (NULL);
+	}
+
+
+	i = 0;
+	while (count < size)
+	{
+		copy->array[count] = NULL;
+		count++;
+	}
+
+	copy->size = size;
+	return (copy);
+}
