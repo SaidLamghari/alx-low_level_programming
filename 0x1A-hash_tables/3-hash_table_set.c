2dec985cebe7ad39fe18e18ff1d223d2783edb80
SaidLamghari <said.lmg@gmail.com>
2023-12-21 15:54:42 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,28 +11,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *ptr;
+	hash_node_t *p;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (key == NULL || ht == NULL || value == NULL || *key == '\0')
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	ptr = ht->array[index];
-	while (ptr != NULL)
+	p = ht->array[index];
+	while (p != NULL)
 	{
-		if (strcmp(ptr->key, key) == 0)
+		if (strcmp(p->key, key) == 0)
 		{
-			free(ptr->value);
-			ptr->value = strdup(value);
+			free(p->value);
+			p->value = strdup(value);
 			return (1);
 		}
-		ptr = ptr->next;
+		p = p->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	new_node->key = strdup(key);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
