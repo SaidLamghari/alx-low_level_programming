6e07dacfa85ecacd4c322ad0794b3cfe86209d80
SaidLamghari <said.lmg@gmail.com>
2023-12-22 02:17:47 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,14 +15,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	/* Allocate memory */
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
-		return NULL;
+		return (NULL);
 	}
 	/* Initialize a element of the array to NULL */
 	count = 0;
