f6de0883ca1059012f6a1b225e0fe5629bac3cd5
SaidLamghari <said.lmg@gmail.com>
2024-02-21 13:33:22 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""function def island_perimeter grid
+that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island in the grid.
+    Args: grid: list of list of integers
+    Returns: integer that is The perimeter of the island.
+    """
+    perimeter = 0
+
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == 1:
+                perimeter += 4
+
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
+
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+
+    return (perimeter)
